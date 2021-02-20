dcb7ef69e28ebce6ee576001c5cbbcdb449da3f7
daniel <leinadlm95@gmail.com>
2021-02-20 17:02:45 -0600

    95. Creando un TabNavigation

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -3,15 +3,28 @@ import {createStackNavigator} from '@react-navigation/stack';
 import About from '../screens/About';
 import Contact from '../screens/Contact';
 import Home from '../screens/Home';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="home" component={Home} options={{title: 'Home'}} />
-      <Stack.Screen name="about" component={About} options={{title: 'About'}} />
-      <Stack.Screen name="contact" component={Contact} options={{title: 'Contact'}} />
-    </Stack.Navigator>
+    <Tab.Navigator>
+      <Tab.Screen name="home" component={Home} options={{title: 'Home'}} />
+      <Tab.Screen name="about" component={Home} options={{title: 'About'}} />
+      <Tab.Screen name="contact" component={Home} options={{title: 'Contact'}} />
+    </Tab.Navigator>
   );
 }
+
+// const Stack = createStackNavigator()
+
+// export default function Navigation() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="home" component={Home} options={{title: 'Home'}} />
+//       <Stack.Screen name="about" component={About} options={{title: 'About'}} />
+//       <Stack.Screen name="contact" component={Contact} options={{title: 'Contact'}} />
+//     </Stack.Navigator>
+//   );
+// }
