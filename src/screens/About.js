373b9e297ba27f675281099388c572e3dd4e25f6
daniel <leinadlm95@gmail.com>
2021-02-20 16:51:29 -0600

    94. Navegando entre screens.

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Button } from 'react-native'
 
-export default function About() {
+export default function About(props) {
+    const {navigation} = props
     return (
         <View>
             <Text>Estamos en About</Text>
+            <Button title="Contact" onPress={()=> navigation.navigate('contact')}/>
         </View>
     )
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import {Button, Text, View} from 'react-native';
 
-export default function Home() {
-    return (
-        <View>
-            <Text>Estamos en Home</Text>
-        </View>
-    )
+export default function Home(props) {
+  console.log(props);
+  const {navigation} = props;
+  return (
+    <View>
+      <Text>Estamos en Home</Text>
+      <Button title="Ir a about" onPress={() => navigation.navigate('about')} />
+      <Button
+        title="Ir a Contact"
+        onPress={() => navigation.navigate('contact')}
+      />
+    </View>
+  );
 }
